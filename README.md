fd1c47bb1c8257bdc646e0a69fbc077db698f781
SammaySarkar <smmysrkr@gmail.com>
2016-02-29 22:18:40 +0600

    indent with spaces. (2 spaces)

--- README.md
+++ README.md
@@ -4,48 +4,48 @@
 *(forked from https://github.com/Binocular222/XYplorer )*
 
 - ####INSTALL:
-	+ **With [Package Control](https://packagecontrol.io/)**
-		- Open Command Palette (<kbd>CTRL</kbd>+<kbd>SHIFT</kbd>+<kbd>P</kbd>), pick `Package Control: Add Repository`
-		- Enter `https://github.com/SammaySarkar/XYplorer` as the Repository URL
-		- Now do `Package Control: Install Package`, search for and install `XYplorer`
-	+ **Without Package Control**
-		- Download this repo as [zip](https://github.com/SammaySarkar/XYplorer/archive/master.zip).
-		  Extract and rename the *folder inside the zip file* to `XYplorer` and move to `Data\Packages\ `.
-		- ALTERNATIVELY, and *only if running Sublime Text 3*, zip up *the contents inside* that `XYplorer` folder,
-		  rename the zip file to `XYplorer.sublime-package` and place it in `Data\Installed Packages\ `.
+  + **With [Package Control](https://packagecontrol.io/)**
+    - Open Command Palette (<kbd>CTRL</kbd>+<kbd>SHIFT</kbd>+<kbd>P</kbd>), pick `Package Control: Add Repository`
+    - Enter `https://github.com/SammaySarkar/XYplorer` as the Repository URL
+    - Now do `Package Control: Install Package`, search for and install `XYplorer`
+  + **Without Package Control**
+    - Download this repo as [zip](https://github.com/SammaySarkar/XYplorer/archive/master.zip).
+      Extract and rename the *folder inside the zip file* to `XYplorer` and move to `Data\Packages\ `.
+    - ALTERNATIVELY, and *only if running Sublime Text 3*, zip up *the contents inside* that `XYplorer` folder,
+      rename the zip file to `XYplorer.sublime-package` and place it in `Data\Installed Packages\ `.
 
 
 - ####SETUP & USAGE:
-	+ Open XYplorer package user settings: `Preferences > Package Settings > XYplorer > Settings - User`
-	+ Save the settings file with following content, using correct path to your XYplorer installation folder.
-	```js
-	{
-	"xypath": "P:\\ath\\to\\XYplorer"
-	"xyscripts": "P:\\ath\\to\\XYScripts"
-	}
-	```
-	+ Do the same with `Preferences > Package Settings > XYplorer > Build Settings - User` and this content.
-	```js
-	{
-	"cmd": ["P:\\ath\\to\\XYplorer\\XYplorer.exe", "", "/script=$file", "/flg=2"],
-	"selector": "source.xys"
-	}
-	```
-	+ The highlighter automatically activates in `*.xys, *.xyi` files.
-	  To include inc files, add an `extensions` key to XYplorer package user settings: `"extensions": ["inc"]`
-	+ Build-system integration is available. Doing `Tools > Build` will run the current script file in XYplorer.
-	+ **Contextual help**
-		- F1 triggers contextual help. Try it on everything: keywords, commandIDs, user-defined functions ...
-			+ Command ID: associated menu caption is displayed in statusbar
-			+ UDF, Namespace: Go to Definition/Declaration. (see UDF note below)
-			+ INCLUDE statement: goto included file (*remember to set corect 'xyscripts' path in package settings*)
-			+ Other language structures: opens associated location in XYplorer help file
-		- UDF note: add include files to project before you can jump to functions defined in those files.
-	+ **Tips**
-		- SubScripts, User-Defined Functions and Namespace declarations are listed in Symbols.
-		- To assign XYplorer icon to .xys files in sidebar:
-			- Create an XYplorer icon in png format and save to `\Data\Packages\Theme - Default\icons\file_type_xys.png`
-			- an example png icon is supplied with this package.
-	+ Some features like Goto Definition, SymbolList etc are not available in ST2
+  + Open XYplorer package user settings: `Preferences > Package Settings > XYplorer > Settings - User`
+  + Save the settings file with following content, using correct path to your XYplorer installation folder.
+  ```js
+  {
+  "xypath": "P:\\ath\\to\\XYplorer"
+  "xyscripts": "P:\\ath\\to\\XYScripts"
+  }
+  ```
+  + Do the same with `Preferences > Package Settings > XYplorer > Build Settings - User` and this content.
+  ```js
+  {
+  "cmd": ["P:\\ath\\to\\XYplorer\\XYplorer.exe", "", "/script=$file", "/flg=2"],
+  "selector": "source.xys"
+  }
+  ```
+  + The highlighter automatically activates in `*.xys, *.xyi` files.
+    To include inc files, add an `extensions` key to XYplorer package user settings: `"extensions": ["inc"]`
+  + Build-system integration is available. Doing `Tools > Build` will run the current script file in XYplorer.
+  + **Contextual help**
+    - F1 triggers contextual help. Try it on everything: keywords, commandIDs, user-defined functions ...
+      + Command ID: associated menu caption is displayed in statusbar
+      + UDF, Namespace: Go to Definition/Declaration. (see UDF note below)
+      + INCLUDE statement: goto included file (*remember to set corect 'xyscripts' path in package settings*)
+      + Other language structures: opens associated location in XYplorer help file
+    - UDF note: add include files to project before you can jump to functions defined in those files.
+  + **Tips**
+    - SubScripts, User-Defined Functions and Namespace declarations are listed in Symbols.
+    - To assign XYplorer icon to .xys files in sidebar:
+      - Create an XYplorer icon in png format and save to `\Data\Packages\Theme - Default\icons\file_type_xys.png`
+      - an example png icon is supplied with this package.
+  + Some features like Goto Definition, SymbolList etc are not available in ST2
 
 *Happy XYScripting!*
--- contextual_help_xys.py
+++ contextual_help_xys.py
@@ -3,67 +3,67 @@ from os import path
 ST2 = (sublime.version()[0] == '2')
 
 class contextual_help_xys(sublime_plugin.TextCommand):
-	def run(self, edit):
-		# import spdb ; spdb.start()
-		xypath = self.view.settings().get('xypath')
-		CursorLocation = self.view.sel()[0]
-		ScopeName = self.view.scope_name(CursorLocation.a).strip()
-		ScopeText = self.view.substr(self.view.extract_scope(CursorLocation.a))
-		if 'string.other.INC.xys' in ScopeName: # goto include file
-			IncludeFile = path.expandvars(ScopeText)
-			#strip syntactical quotes
-			if (IncludeFile[0] == IncludeFile[-1]) and (IncludeFile[0] in ['"',"'"]):
-				IncludeFile = IncludeFile.strip('"\'')
-			if not path.isabs(IncludeFile):
-				xyscripts = self.view.settings().get('xyscripts')
-				if not xyscripts:	# TODO: get scripts path from startup.ini
-					xyscripts = path.expandvars(xypath).rstrip('\\') + '\\Data\\Scripts' # default for now
-				IncludeFile = path.expandvars(xyscripts.rstrip('\\') + '\\' + IncludeFile)
-			if path.exists(IncludeFile):	self.view.window().open_file(IncludeFile)
-			else:	sublime.status_message('Cannot open file: ' + IncludeFile)
-		elif ScopeName == 'source.xys entity.name.function.UDF.xys':
-			# need to handle namespaces
-			self.view.window().run_command('goto_definition', {'symbol' : 'UDF: ' + ScopeText.lower()})
-		elif ScopeName == 'source.xys entity.name.section.NS.xys':
-			self.view.window().run_command('goto_definition', {'symbol' : 'NS: ' + ScopeText.lower()})
-		elif ScopeName == 'source.xys entity.name.function.xys':
-			subprocess.Popen(['hh.exe', xypath + '\\XYplorer.chm::/idh_scripting_comref.htm#idh_sc_' + ScopeText])
-		elif 'variable.parameter.xys' in ScopeName:
-			subprocess.Popen(['hh.exe', xypath + '\\XYplorer.chm::/idh_scripting.htm#idh_scripting_variables'])
-		elif 'variable.parameter.native.xys' in ScopeName:
-			subprocess.Popen(['hh.exe', xypath + '\\XYplorer.chm::/idh_variables.htm'])
-		elif 'string.other.scriptcaption.xys' in ScopeName:
-			subprocess.Popen(['hh.exe', xypath + '\\XYplorer.chm::/idh_scripting.htm#idh_scripting_xysadv'])
-		elif ScopeName == 'source.xys keyword.operator.xys':
-			subprocess.Popen(['hh.exe', xypath + '\\XYplorer.chm::/idh_scripting.htm#idh_scripting_operatorprecedence'])
-		elif ScopeName == 'source.xys string.unquoted.heredoc.xys' or ScopeName == 'source.xys string.unquoted.nowdoc.xys':
-			subprocess.Popen(['hh.exe', xypath + '\\XYplorer.chm::/idh_scripting.htm#idh_scripting_heredoc'])
-		elif ScopeName == 'source.xys keyword.control.xys':
-			ScopeTextLower = ScopeText.lower().strip('({')
-			if ScopeTextLower == 'if' or ScopeTextLower == 'elseif' or ScopeTextLower == 'else':
-				subprocess.Popen(['hh.exe', xypath + '\\XYplorer.chm::/idh_scripting.htm#idh_scripting_ifthen'])
-			elif ScopeTextLower == 'while' or ScopeTextLower == 'foreach':
-				subprocess.Popen(['hh.exe', xypath + '\\XYplorer.chm::/idh_scripting.htm#idh_scripting_' + ScopeTextLower + 'loops'])
-			elif ScopeTextLower == 'step' or ScopeTextLower == 'unstep' \
-					or ScopeTextLower == 'break' or ScopeTextLower == 'continue':
-				subprocess.Popen(['hh.exe', xypath + '\\XYplorer.chm::/idh_scripting_comref.htm#idh_sc_' + ScopeText])
-			else:
-				subprocess.Popen(['hh.exe', xypath + '\\XYplorer.chm::/idh_scripting.htm'])
-		elif ScopeName == 'source.xys entity.name.function.CommandID.xys':
-			if ST2:
-				cmdIDList = open(sublime.packages_path() + '\\XYplorer\\cmdIDs.txt', 'r')
-				CmdDict = eval( '{' + cmdIDList.read() + '}' ); cmdIDList.close()
-			else:
-				CmdDict = eval( '{' + sublime.load_resource('Packages/XYplorer/cmdIDs.txt') + '}' )
-			if ScopeText in CmdDict:
-				sublime.status_message(CmdDict[ScopeText])
-			else:
-				sublime.status_message(ScopeText + ': Command ID not recognized')
-		else:
-			# Contextual_help is assigned to ctrl+enter, which coincides with
-			# ST's kb shortcut for new line macro. This is to compensate.
-			self.view.window().run_command('move_to', {'to': 'hardeol'})        # go to line end
-			self.view.window().run_command('insert', {'characters': '\n'})      # Add a new line
+  def run(self, edit):
+    # import spdb ; spdb.start()
+    xypath = self.view.settings().get('xypath')
+    CursorLocation = self.view.sel()[0]
+    ScopeName = self.view.scope_name(CursorLocation.a).strip()
+    ScopeText = self.view.substr(self.view.extract_scope(CursorLocation.a))
+    if 'string.other.INC.xys' in ScopeName: # goto include file
+      IncludeFile = path.expandvars(ScopeText)
+      #strip syntactical quotes
+      if (IncludeFile[0] == IncludeFile[-1]) and (IncludeFile[0] in ['"',"'"]):
+        IncludeFile = IncludeFile.strip('"\'')
+      if not path.isabs(IncludeFile):
+        xyscripts = self.view.settings().get('xyscripts')
+        if not xyscripts: # TODO: get scripts path from startup.ini
+          xyscripts = path.expandvars(xypath).rstrip('\\') + '\\Data\\Scripts' # default for now
+        IncludeFile = path.expandvars(xyscripts.rstrip('\\') + '\\' + IncludeFile)
+      if path.exists(IncludeFile):  self.view.window().open_file(IncludeFile)
+      else: sublime.status_message('Cannot open file: ' + IncludeFile)
+    elif ScopeName == 'source.xys entity.name.function.UDF.xys':
+      # need to handle namespaces
+      self.view.window().run_command('goto_definition', {'symbol' : 'UDF: ' + ScopeText.lower()})
+    elif ScopeName == 'source.xys entity.name.section.NS.xys':
+      self.view.window().run_command('goto_definition', {'symbol' : 'NS: ' + ScopeText.lower()})
+    elif ScopeName == 'source.xys entity.name.function.xys':
+      subprocess.Popen(['hh.exe', xypath + '\\XYplorer.chm::/idh_scripting_comref.htm#idh_sc_' + ScopeText])
+    elif 'variable.parameter.xys' in ScopeName:
+      subprocess.Popen(['hh.exe', xypath + '\\XYplorer.chm::/idh_scripting.htm#idh_scripting_variables'])
+    elif 'variable.parameter.native.xys' in ScopeName:
+      subprocess.Popen(['hh.exe', xypath + '\\XYplorer.chm::/idh_variables.htm'])
+    elif 'string.other.scriptcaption.xys' in ScopeName:
+      subprocess.Popen(['hh.exe', xypath + '\\XYplorer.chm::/idh_scripting.htm#idh_scripting_xysadv'])
+    elif ScopeName == 'source.xys keyword.operator.xys':
+      subprocess.Popen(['hh.exe', xypath + '\\XYplorer.chm::/idh_scripting.htm#idh_scripting_operatorprecedence'])
+    elif ScopeName == 'source.xys string.unquoted.heredoc.xys' or ScopeName == 'source.xys string.unquoted.nowdoc.xys':
+      subprocess.Popen(['hh.exe', xypath + '\\XYplorer.chm::/idh_scripting.htm#idh_scripting_heredoc'])
+    elif ScopeName == 'source.xys keyword.control.xys':
+      ScopeTextLower = ScopeText.lower().strip('({')
+      if ScopeTextLower == 'if' or ScopeTextLower == 'elseif' or ScopeTextLower == 'else':
+        subprocess.Popen(['hh.exe', xypath + '\\XYplorer.chm::/idh_scripting.htm#idh_scripting_ifthen'])
+      elif ScopeTextLower == 'while' or ScopeTextLower == 'foreach':
+        subprocess.Popen(['hh.exe', xypath + '\\XYplorer.chm::/idh_scripting.htm#idh_scripting_' + ScopeTextLower + 'loops'])
+      elif ScopeTextLower == 'step' or ScopeTextLower == 'unstep' \
+          or ScopeTextLower == 'break' or ScopeTextLower == 'continue':
+        subprocess.Popen(['hh.exe', xypath + '\\XYplorer.chm::/idh_scripting_comref.htm#idh_sc_' + ScopeText])
+      else:
+        subprocess.Popen(['hh.exe', xypath + '\\XYplorer.chm::/idh_scripting.htm'])
+    elif ScopeName == 'source.xys entity.name.function.CommandID.xys':
+      if ST2:
+        cmdIDList = open(sublime.packages_path() + '\\XYplorer\\cmdIDs.txt', 'r')
+        CmdDict = eval( '{' + cmdIDList.read() + '}' ); cmdIDList.close()
+      else:
+        CmdDict = eval( '{' + sublime.load_resource('Packages/XYplorer/cmdIDs.txt') + '}' )
+      if ScopeText in CmdDict:
+        sublime.status_message(CmdDict[ScopeText])
+      else:
+        sublime.status_message(ScopeText + ': Command ID not recognized')
+    else:
+      # Contextual_help is assigned to ctrl+enter, which coincides with
+      # ST's kb shortcut for new line macro. This is to compensate.
+      self.view.window().run_command('move_to', {'to': 'hardeol'})        # go to line end
+      self.view.window().run_command('insert', {'characters': '\n'})      # Add a new line
 
 # ::/idh_scripting_comref.htm#idh_sc_folderreport
 # mk:@MSITStore:C:\XYplorer\XYplorer.chm::/idh_scripting_comref.htm#idh_sc_getpathcomponent
